3880b4678fccb8b111edcb6bc6994af671965584
Administrator <1290414968@qq.com>
2018-06-03 18:24:54 +0800

    分布式基础提交

--- src/main/java/rmi/HelloImpl.java
+++ src/main/java/rmi/HelloImpl.java
@@ -5,6 +5,7 @@ import java.rmi.server.UnicastRemoteObject;
 
 public class HelloImpl extends UnicastRemoteObject implements  IHello {
     protected HelloImpl() throws RemoteException {
+        super();
     }
     public void say(String msg) throws RemoteException {
         System.out.println("mmmm--->"+msg);
